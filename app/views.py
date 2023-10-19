a7193129c9f072806eb835366e40d36660494b2a
sangreguerrer <san_guerrero@icloud.com>
2023-10-19 23:43:19 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,31 @@
+import datetime
+import os
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    if os.path.exists('first_project'):
+        workdir = os.listdir('first_project')
+        return HttpResponse(workdir)
+    raise NotImplemented
